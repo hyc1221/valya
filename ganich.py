54997b8d8770ce82f2e5f213585da69526c3208d
hyc1221 <hyc1221@yandex.ru>
2021-06-25 16:56:33 +0300

    phrases added

--- ganich.py
+++ ganich.py
@@ -6,22 +6,23 @@ import os
 import random
 from discord.ext import commands
 from config import settings
+from text import phrases, send_hello, send_join_error
 
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.event
 async def on_ready():
-    print('я не сплю')
+    print(phrases['on_ready'])
 
 @bot.command() # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
 async def hello(ctx): # Создаём функцию и передаём аргумент ctx.
     author = ctx.message.author # Объявляем переменную author и записываем туда информацию об авторе.
-    print(f'Уважаемый, {author.name}. Хочу вас приветствовать и поприветствовать.')
-    await ctx.send(f'Уважаемый, {author.mention}. Хочу вас приветствовать и поприветствовать.') # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
+    print(send_hello(author.name))
+    await ctx.send(send_hello(author.mention)) # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
 
 @bot.command()
 async def fox(ctx):
-    response = requests.get('https://some-random-api.ml/img/cat')
+    response = requests.get('https://some-random-api.ml/img/fox')
     json_data = json.loads(response.text)
     embed = discord.Embed(color = 0xff990, title = 'Random fox')
     embed.set_image(url = json_data['link'])
@@ -32,7 +33,7 @@ async def fox(ctx):
 async def cutie(ctx):
     images = glob.glob('img/*jpg')
     image = random.choice(images)
-    print('random ganich')
+    print(phrases['cutie_debug'])
     await ctx.send(file=discord.File(image))
 
 @bot.command()
@@ -41,14 +42,21 @@ async def join(ctx):
     if connected:
         print(ctx.author.voice.channel.id, ctx.author.voice.channel.name)
         await connected.channel.connect()
+        await ctx.send(phrases['join_connect'])
     else: 
-        print(f'Уважаемый {ctx.author.name}, сначала сам зайди, потом зови. (ты никто и звать тебя никта)')
-        await ctx.send(f'Уважаемый {ctx.author.mention}, сначала сам зайди, потом зови. (ты никто и звать тебя никта)')
+        print(send_join_error(ctx.author.name))
+        await ctx.send(send_join_error(ctx.author.mention))
+
+@bot.command()
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send(phrases['leave'])
 
 @bot.command()
 async def ping(ctx):
     print(ctx.author.name, 'ping')
-    await ctx.send('Руки убрал, РУКИ УБРАЛ Я СКАЗАЛ! Трогать будешь свою Палину бл*ть.', file=discord.File(r'd:/py/valya/img/ping.gif'))
+    await ctx.send(phrases['ping'], file=discord.File(r'd:/py/valya/img/ping.gif'))
 
 
 bot.run(settings['token']) # Обращаемся к словарю settings с ключом token, для получения токена
